db0c551388a5b399e840bbd5b0f2804d286da058
alan.liu <a0916758365@gmail.com>
2017-10-11 17:58:17 +0800

    Create Console Application

---
+++ dotnetconf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetconf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
